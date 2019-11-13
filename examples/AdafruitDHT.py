e235cb94f4b090570239b3fd49bcbe10ba0e2be8
sean54017 <seanchen54017@gmail.com>
2019-11-13 10:54:30 +0800

    mcs

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -22,6 +22,31 @@
 import sys
 
 import Adafruit_DHT
+import time
+import http.client as http
+import urllib
+import json
+
+deviceid="Dbod01o0"
+deviceKey="5xO9OGH0eZafS6Zp"
+def post_to_mcs(payload):
+	headers={"Content-type":"application/json","deviceKey":deviceKey}
+	not_connected=1
+	while(not_connected):
+		try:
+			conn=http.HTTPConnection("api.mediatek.com:80")
+			conn.connect()
+			not_connect = 0
+		except(http.HTTPException,scoket.error)as ex:
+			print("Error:%s"%ex)
+			time.sleep(10)
+			#sleep 10 seconds
+	conn.request("POST","/mcs/v2/devices/" + deviced + "/datapoints",json.dumps(payload),headers)
+	response = conn.getresponse()
+	print(response.status, response.reason, json.dumps(payload), time.strftime("%c"))
+	data = response.read()
+	conn.close()
+
 
 
 # Parse command line parameters.
@@ -56,6 +81,9 @@ while True:
 	h0,t0=Adafruit_DHT.read_retry(sensor,pin)
 	if h0 is not None and t0 is not None:
 		print('Temp={0:0.1f}* Humi={1:0.1f}%'.format(t0, h0))
+		payload={"datapoints":[{"dataChnld":"Humidity","values":{"values":ho}},{"dataChnld":"Temperature","values":{"values":to}}]}
+		post_to_mcs(payload)
+		time.sleep(10)
 	else:
 		print('Failed to get reading, Try again!')
 		sys.exit(1)
